346e115ab1f886adabff67f7b65b28dbf4cb991a
Marisa Espinoza <marisaa94@gmail.com>
2023-05-03 20:47:04 -0700

    starting my files for my clock project

---
+++ clock.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', event => {
+displayTime();
+})
+
+function displayTime() {
+
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+}
