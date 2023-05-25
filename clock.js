3fce9da4660332c4c8e08acef299a57e1db94898
Marisa Espinoza <marisaa94@gmail.com>
2023-05-24 20:50:05 -0700

    updated my start and stop button functions to start and stop the clock

--- clock.js
+++ clock.js
@@ -1,12 +1,13 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-    let intervalId = setInterval(displayTime, 1000);
+  startClock();
 
-    //stop clock
-    let stopButton = document.getElementById("stop-button");
-    stopButton.addEventListener('click', event => {
-        clearInterval(intervalId);
-        stopButton.style.backgroundColor = 'red';
-    })
+  //stop clock
+  let stopButton = document.getElementById("stop-button");
+  stopButton.addEventListener("click", stopClock);
+
+  //restart clock
+  let startButton = document.getElementById("start-button");
+  startButton.addEventListener("click", restartClock);
 });
 
 function displayTime() {
@@ -14,26 +15,46 @@ function displayTime() {
   let hours = date.getHours();
   let minutes = date.getMinutes();
   let seconds = date.getSeconds();
-let session = "AM";
+  let session = "AM";
 
-if (hours == 0) {
+  if (hours == 0) {
     hours = 12;
-}
+  }
 
-if (hours > 12) {
+  if (hours > 12) {
     hours = hours - 12;
     session = "PM";
+  }
+
+  hours = hours < 10 ? `0${hours}` : hours;
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  let time = `${hours}:${minutes}:${seconds} ${session}`;
+
+  let displayClock = document.getElementById("clock-face");
+
+  displayClock.innerHTML = time;
 }
 
-hours = (hours < 10) ? `0${hours}` : hours;
-minutes = (minutes < 10) ? `0${minutes}` : minutes;
-seconds = (seconds < 10) ? `0${seconds}` : seconds;
+let isClockRunning = false;
+let intervalId;
 
-let time = `${hours}:${minutes}:${seconds} ${session}`
-
-let displayClock = document.getElementById('clock-face');
-
-displayClock.innerHTML = time;
-
+//start clock
+function startClock() {
+  intervalId = setInterval(displayTime, 1000);
+  isClockRunning = true;
 }
 
+//stop clock
+function stopClock() {
+  clearInterval(intervalId);
+  isClockRunning = false;
+  document.getElementById("stop-button").style.backgroundColor = "red";
+}
+
+//restart clock
+function restartClock() {
+  document.getElementById("stop-button").style.backgroundColor = "";
+  startClock();
+}
